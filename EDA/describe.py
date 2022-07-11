8f0c78b069de95d301696b7564fd5bc7a2679755
Ayush Mishra <91671487+ayushunofficial@users.noreply.github.com>
2022-07-11 23:30:25 +0530

    Create describe.py

---
+++ EDA/describe.py
@@ -0,0 +1,16 @@
+
+import pandas  as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+%matplotlib inline
+
+plt.rcParams['figure.figsize'] = [8,5]
+plt.rcParams['font.size'] =14
+plt.rcParams['font.weight']= 'bold'
+plt.style.use('seaborn-whitegrid')
+
+df = pd.read_csv('./dataset/Fish.csv')
+
+df.describe()
